a514f1945fd5c89072b294e835ac4916986dff29
Gavin Sun <gavinsun0921@foxmail.com>
2023-08-31 13:39:57 +0800

    add 'SoftmaxCrossEntropyLoss' from official/cv/DeepLabV3P/src/loss.py

--- src/Criterion.py
+++ src/Criterion.py
@@ -93,6 +93,40 @@ class CrossEntropyWithLogits(nn.LossBase):
         return loss
 
 
+class SoftmaxCrossEntropyLoss(nn.Cell):
+    """SoftmaxCrossEntropyLoss"""
+
+    def __init__(self, num_cls=21, ignore_label=255):
+        super(SoftmaxCrossEntropyLoss, self).__init__()
+        self.one_hot = P.OneHot(axis=-1)
+        self.on_value = Tensor(1.0, mstype.float32)
+        self.off_value = Tensor(0.0, mstype.float32)
+        self.cast = P.Cast()
+        self.ce = nn.SoftmaxCrossEntropyWithLogits()
+        self.not_equal = P.NotEqual()
+        self.num_cls = num_cls
+        self.ignore_label = ignore_label
+        self.mul = P.Mul()
+        self.sum = P.ReduceSum(False)
+        self.div = P.RealDiv()
+        self.transpose = P.Transpose()
+        self.reshape = P.Reshape()
+
+    def construct(self, logits, labels):
+        """SoftmaxCrossEntropyLoss.construct"""
+        labels_int = self.cast(labels, mstype.int32)
+        labels_int = self.reshape(labels_int, (-1,))
+        logits_ = self.transpose(logits, (0, 2, 3, 1))
+        logits_ = self.reshape(logits_, (-1, self.num_cls))
+        weights = self.not_equal(labels_int, self.ignore_label)
+        weights = self.cast(weights, mstype.float32)
+        one_hot_labels = self.one_hot(labels_int, self.num_cls, self.on_value, self.off_value)
+        loss = self.ce(logits_, one_hot_labels)
+        loss = self.mul(weights, loss)
+        loss = self.div(self.sum(loss), self.sum(weights))
+        return loss
+
+
 class Lovasz_hinge(LossBase):
     '''
     This is the autograd version, used in the multi-category classification case
